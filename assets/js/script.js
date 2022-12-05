a38df9bba51296f20fe5c6bd582b10218abe8ed9
GazzaW6 <garrettwhyte@gmail.com>
2022-12-05 20:03:06 +0000

    add javascript code for computer choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,27 @@ let userChoiceDisplay= document.getElementById("user-choice");
 let resultDisplay= document.getElementById("result");
 let possibleChoices= document.querySelectorAll("button");
 let userChoice
+
 //generate user choice for game //
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
   userChoice = e.target.id
   userChoiceDisplay.innerHTML = userChoice
-}))
+  generateComputerChoice()
+}));
+
+//generate a random computer choice //
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length)
+    if (randomNumber === 1) {
+      computerChoice = 'rock'
+    }
+    if (randomNumber === 2) {
+      computerChoice = 'scissors'
+    }
+    if (randomNumber === 3) {
+      computerChoice = 'paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+  }
+  
