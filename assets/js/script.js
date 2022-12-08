edde30082e6d0c6f9b54afefccc3b3fb6a278893
GazzaW6 <garrettwhyte@gmail.com>
2022-12-08 20:41:33 +0000

    add semi-colons and fix cover image text to java script

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,11 @@ let result
 
 //generate user choice for game //
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-  userChoice = e.target.id
-  userChoiceDisplay.innerHTML = userChoice
-  generateComputerChoice()
-  getResult()
-  changeImage()
+  userChoice = e.target.id;
+  userChoiceDisplay.innerHTML = userChoice;
+  generateComputerChoice();
+  getResult();
+  changeImage();
 }));
 
 //generate a random computer choice //
@@ -24,19 +24,19 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
 function generateComputerChoice() {
     const randomNumber = Math.floor(Math.random() * possibleChoices.length)
     if (randomNumber === 0) {
-      computerChoice = 'Rock'
+      computerChoice = 'Rock';
     }
     if (randomNumber === 1) {
-      computerChoice = 'Scissors'
+      computerChoice = 'Scissors';
     }
     if (randomNumber === 2) {
-      computerChoice = 'Paper'
+      computerChoice = 'Paper';
     }
     if (randomNumber === 3) {
-        computerChoice = 'Spock'
+        computerChoice = 'Spock';
     }
     if (randomNumber === 4) {
-        computerChoice = 'Lizard'
+        computerChoice = 'Lizard';
     } 
 
     computerChoiceDisplay.innerHTML = computerChoice
@@ -46,153 +46,153 @@ function generateComputerChoice() {
 
 function getResult() {
     if (computerChoice === userChoice) {
-      result = "It's a draw!"
-      var image= document.getElementById("computer-image")
+      result = "It's a draw!";
+      var image= document.getElementById("computer-image");
       image.src="assets/images/cover image.png";
-      var image=document.getElementById("user-image")
+      var image=document.getElementById("user-image");
       image.src="assets/images/cover image.png";
     }
     if (computerChoice === "Rock" && userChoice === "Paper") {
-      result = "You win!"
-      var image= document.getElementById("computer-image")
+      result = "You win!";
+      var image= document.getElementById("computer-image");
       image.src="assets/images/rock.png";
-      var image=document.getElementById("user-image")
+      var image=document.getElementById("user-image");
       image.src="assets/images/paper.png";
     }
     if (computerChoice === "Rock" && userChoice === "Scissors") {
-      result = "You lost! Try again."
-      var image= document.getElementById("computer-image")
+      result = "You lost! Try again.";
+      var image= document.getElementById("computer-image");
       image.src="assets/images/rock.png";
-      var image=document.getElementById("user-image")
+      var image=document.getElementById("user-image");
       image.src="assets/images/scissors.png";
     }
     if (computerChoice === "Rock" && userChoice === "Lizard") {
-        result = "You lost! Try again."
-        var image= document.getElementById("computer-image")
+        result = "You lost! Try again.";
+        var image= document.getElementById("computer-image");
         image.src="assets/images/rock.png";
-        var image=document.getElementById("user-image")
+        var image=document.getElementById("user-image");
         image.src="assets/images/lizard.png";
       }
       if (computerChoice === "Rock" && userChoice === "Spock") {
-        result = "You win!"
-        var image= document.getElementById("computer-image")
+        result = "You win!";
+        var image= document.getElementById("computer-image");
         image.src="assets/images/rock.png";
-        var image=document.getElementById("user-image")
+        var image=document.getElementById("user-image");
         image.src="assets/images/spock.png";
       }
     if (computerChoice === "Paper" && userChoice === "Scissors") {
-      result = "You win!"
-      var image= document.getElementById("computer-image")
+      result = "You win!";
+      var image= document.getElementById("computer-image");
       image.src="assets/images/paper.png";
-      var image=document.getElementById("user-image")
+      var image=document.getElementById("user-image");
       image.src="assets/images/scissors.png";
     }
     if (computerChoice === 'Paper' && userChoice === "Rock") {
-      result = "You lost! Try again."
-      var image= document.getElementById("computer-image")
+      result = "You lost! Try again.";
+      var image= document.getElementById("computer-image");
       image.src="assets/images/paper.png";
-      var image=document.getElementById("user-image")
+      var image=document.getElementById("user-image");
       image.src="assets/images/rock.png";
     }
     if (computerChoice === 'Paper' && userChoice === "Lizard") {
-        result = "You win!"
-        var image= document.getElementById("computer-image")
+        result = "You win!";
+        var image= document.getElementById("computer-image");
         image.src="assets/images/paper.png";
-        var image=document.getElementById("user-image")
+        var image=document.getElementById("user-image");
         image.src="assets/images/lizard.png";
       }
       if (computerChoice === 'Paper' && userChoice === "Spock") {
-        result = "You lost! Try again."
-        var image= document.getElementById("computer-image")
+        result = "You lost! Try again.";
+        var image= document.getElementById("computer-image");
         image.src="assets/images/paper.png";
-        var image=document.getElementById("user-image")
+        var image=document.getElementById("user-image");
         image.src="assets/images/spock.png";
       }
     if (computerChoice === 'Scissors' && userChoice === "Rock") {
-      result = "You win!"
-      var image= document.getElementById("computer-image")
+      result = "You win!";
+      var image= document.getElementById("computer-image");
       image.src="assets/images/scissors.png";
-      var image=document.getElementById("user-image")
+      var image=document.getElementById("user-image");
       image.src="assets/images/rock.png";
     }
     if (computerChoice === 'Scissors' && userChoice === "Paper") {
-      result = "You lost! Try again."
-      var image= document.getElementById("computer-image")
+      result = "You lost! Try again.";
+      var image= document.getElementById("computer-image");
       image.src="assets/images/scissors.png";
-      var image=document.getElementById("user-image")
+      var image=document.getElementById("user-image");
       image.src="assets/images/paper.png";
     }
     if (computerChoice === 'Scissors' && userChoice === "Lizard") {
         result = "You lost! Try again."
-        var image= document.getElementById("computer-image")
+        var image= document.getElementById("computer-image");
         image.src="assets/images/scissors.png";
-        var image=document.getElementById("user-image")
+        var image=document.getElementById("user-image");
         image.src="assets/images/lizard.png";
       }
       if (computerChoice === 'Scissors' && userChoice === "Spock") {
-        result = "You lost! Try again."
-        var image= document.getElementById("computer-image")
+        result = "You lost! Try again.";
+        var image= document.getElementById("computer-image");
         image.src="assets/images/scissors.png";
-        var image=document.getElementById("user-image")
+        var image=document.getElementById("user-image");
         image.src="assets/images/spock.png";
       }
     if (computerChoice === 'Lizard' && userChoice === "Spock") {
-        result = "You lost! Try again."
-        var image= document.getElementById("computer-image")
+        result = "You lost! Try again.";
+        var image= document.getElementById("computer-image");
         image.src="assets/images/lizard.png";
-        var image=document.getElementById("user-image")
+        var image=document.getElementById("user-image");
         image.src="assets/images/spock.png";
     }
     if (computerChoice === 'Lizard' && userChoice === "Paper") {
-        result = "You lost! Try again."
-        var image= document.getElementById("computer-image")
+        result = "You lost! Try again.";
+        var image= document.getElementById("computer-image");
         image.src="assets/images/lizard.png";
-        var image=document.getElementById("user-image")
+        var image=document.getElementById("user-image");
         image.src="assets/images/paper.png";
     }
     if (computerChoice === 'Lizard' && userChoice === "Rock") {
-        result = "You win!"
-        var image= document.getElementById("computer-image")
+        result = "You win!";
+        var image= document.getElementById("computer-image");
         image.src="assets/images/lizard.png";
-        var image=document.getElementById("user-image")
+        var image=document.getElementById("user-image");
         image.src="assets/images/rock.png";
     }
     if (computerChoice === 'Lizard' && userChoice === "Scissors") {
-        result = "You win!"
-        var image= document.getElementById("computer-image")
+        result = "You win!";
+        var image= document.getElementById("computer-image");
         image.src="assets/images/lizard.png";
-        var image=document.getElementById("user-image")
+        var image=document.getElementById("user-image");
         image.src="assets/images/scissors.png";
     }
     if (computerChoice === 'Spock' && userChoice === "Lizard") {
-        result = "You win! "
-        var image= document.getElementById("computer-image")
+        result = "You win!";
+        var image= document.getElementById("computer-image");
         image.src="assets/images/spock.png";
-        var image=document.getElementById("user-image")
+        var image=document.getElementById("user-image");
         image.src="assets/images/lizard.png";
     }
     if (computerChoice === 'Spock' && userChoice === "Paper") {
-        result = "You win! "
-        var image= document.getElementById("computer-image")
+        result = "You win!";
+        var image= document.getElementById("computer-image");
         image.src="assets/images/spock.png";
-        var image=document.getElementById("user-image")
+        var image=document.getElementById("user-image");
         image.src="assets/images/paper.png";
     }
     if (computerChoice === 'Spock' && userChoice === "Rock") {
-        result = "You lost! Try again."
-        var image= document.getElementById("computer-image")
+        result = "You lost! Try again.";
+        var image= document.getElementById("computer-image");
         image.src="assets/images/spock.png";
-        var image=document.getElementById("user-image")
+        var image=document.getElementById("user-image");
         image.src="assets/images/rock.png";
     }
     if (computerChoice === 'Spock' && userChoice === "Scissors") {
-        result = "You lost! Try again."
-        var image= document.getElementById("computer-image")
+        result = "You lost! Try again.";
+        var image= document.getElementById("computer-image");
         image.src="assets/images/spock.png";
-        var image=document.getElementById("user-image")
+        var image=document.getElementById("user-image");
         image.src="assets/images/scissors.png";
     }
 
 
-    resultDisplay.innerHTML = result
+    resultDisplay.innerHTML = result;
   }
